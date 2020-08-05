6ac8f873e7a5b7f6176b5fa965cb67cdb0229a94
Miles Chen <miles.chen01@sap.com>
2020-08-05 16:17:37 +0800

    2020 powered by thumbsup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This site is built on top of Github Pages.
+thumbsup --theme cards --input ./ --output /home/miles/repo/mc/anjie_2020/bin2 --title Anjie ART Gallery --footer © Anjie ART 2020 --google-analytics UA-11765858-3 --thumb-size 200 --large-size 1200
--- content/scripts/my.js
+++
@@ -1,4 +0,0 @@
-/*!
- * Global scripts for anjieart.net
- */
-(function(){var c=c||[];c.push(["_setAccount","UA-11765858-3"]);c.push(["_trackPageview"]);var b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a);this._gaq=c}).call(window);
